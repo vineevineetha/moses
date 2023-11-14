f909c1d07bb23d5485f61e871616c30d313a9f47
moses <luckymoses7777@gmail.com>
2023-11-14 18:22:53 +0530

    user_create

---
+++ project/delete_user.sh
@@ -0,0 +1,8 @@
+if id $1 &>/dev/null; then
+    # Delete the user
+   sudo  userdel -r $1
+    echo "User '$username' deleted."
+else
+    echo "User '$username' not found."
+fi
+
---
+++ project/user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+    if [ $# != 2 ]; then
+        echo "Usage: create_user <username>"
+        echo "Argument should be exactly 2"
+    else
+        echo "Creating user with name $1..."
+        sudo useradd -m -p $(openssl passwd -1 $2) $1
+
+        if [ $? -eq 0 ]; then
+            echo "Successfully created user: $1"
+        else
+            echo "Failed to create user: $1"
+        fi
+    fi
+
